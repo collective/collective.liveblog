12e4e0d46a3eac8bc57c59f41292e475e1cf2797
hvelarde <hector.velarde@gmail.com>
2014-09-01 11:46:21 -0300

    Add confirmation dialog to delete micro-update action

--- src/collective/liveblog/browser/update.py
+++ src/collective/liveblog/browser/update.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from collective.liveblog import _
 from collective.liveblog.browser.view import View
 from collective.liveblog.interfaces import IBrowserLayer
 from collective.liveblog.interfaces import ILiveblog
 from five import grok
+from zope.i18n import translate
 from zope.security import checkPermission
 
 grok.templatedir('templates')
@@ -18,3 +20,8 @@ class Update(View):
 
     def can_delete_objects(self):
         return checkPermission('zope2.DeleteObjects', self.context)
+
+    def delete_confirmation(self):
+        msg = _(u'Do you really want to delete this item?')
+        msg = translate(msg, 'collective.liveblog', context=self.request)
+        return u"return confirm('{0}')".format(msg)
