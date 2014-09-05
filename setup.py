07b4fb9bb0b594f75f22d643557d59f2c20f87a5
hvelarde <hector.velarde@gmail.com>
2014-09-05 10:45:20 -0300

    Back to development: 1.0b2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0b1'
+version = '1.0b2.dev0'
 description = 'A liveblogging solution for Plone.'
 long_description = (
     open('README.rst').read() + '\n' +
