66e91e77dd0f63886f2a53574bd3d5e73e30ed4a
hvelarde <hector.velarde@gmail.com>
2014-09-05 09:18:36 -0300

    Use local time for timestamp generation

--- src/collective/liveblog/utils.py
+++ src/collective/liveblog/utils.py
@@ -4,6 +4,6 @@ from datetime import datetime
 
 def _timestamp(dt):
     """Return a timestamp from a datetime object."""
-    epoch = datetime.utcfromtimestamp(0)
+    epoch = datetime.fromtimestamp(0)
     delta = dt - epoch
     return str(delta.total_seconds())
